e12fe1f58bc4494a57f93d7e7edd72ca3a59b202
zhihao <zanzhihao@foxmail.com>
2019-06-02 19:07:48 +0800

    initialize data

---
+++ LSTM.R
@@ -0,0 +1,6 @@
+library(readr)
+setwd("c:/users/zanzh/OneDrive/研一下/金融数据分析/LSTM_VS_ARIMA/")
+hs300 = read_csv('data/沪深300.csv',locale=locale(encoding='UTF-8'))
+sz50 = read_csv('data/上证50.csv')
+szcz = read_csv('data/深证.csv')
+zxb = read_csv('data/中小板.csv')
