2b8031f0c7b74c250794919b1a57266a581fc6c5
chihao <542476401@qq.com>
2019-06-07 23:49:40 +0800

    add TODOs

--- arima模型/上证arima.R
+++ arima模型/上证arima.R
@@ -6,17 +6,17 @@ head(da1)
 logSZ50Pri = log((da1[c(168: 2167), 3]))
 head(logSZ50Pri)
 tdx = c(1: 2000)/250 + 2010
-plot(tdx, logSZ50Pri, xlab = 'day', ylab = 'ln(price)', type='l')
+plot(tdx, logSZ50Pri, xlab = 'day', ylab = 'ln(price)', type='l')  #TODO 输出的图放在graph/下面
 title(main = '上证50指数变化')
 dlogSZ50Pri = diff(logSZ50Pri) #做一阶差分
-adf.test(dlogSZ50Pri) # 在5%的显著水平下拒绝原假设，一阶差分后数据平稳
+adf.test(dlogSZ50Pri) # 在5%的显著水平下拒绝原假设，一阶差分后数据平稳  # TODO adf检验的结果保存为文本放到graph/下面
 par(mfcol = c(1, 2))
-acf(dlogSZ50Pri, lag = 20)
+acf(dlogSZ50Pri, lag = 20) #TODO 同上
 pacf(dlogSZ50Pri, lag = 20)
 # 观察数据ar使用3阶，ma采用两阶，建立arima(3, 0, 2)模型
 SZ50_model = arima(dlogSZ50Pri, order = c(3, 0, 2), include.mean = F)
 SZ50_model
-Box.test(SZ50_model$residuals) # p值显著大于0.05，残差为白噪声序列，选取的模型合适，不需要使用garch模型
+Box.test(SZ50_model$residuals) # p值显著大于0.05，残差为白噪声序列，选取的模型合适，不需要使用garch模型  #TODO 检验结果保存
 #计算原本数据的对数收益率
 logr = diff(log(da1[c(2168:2468), 3]))
 # 短期预测
@@ -40,3 +40,7 @@ for(i in 1: 250) {
   long_di_sum = long_di_sum + abs(SZ50_arima_forecast[[4]][i] - logr[i])
 }
 long_di_sum
+#TODO 画图看一下, 更直观
+# 画图样例
+plot(logr,type= 'l',main = "comparison", xlab = 'time series', ylab='log return') # 实际值
+lines(SZ50_arima_forecast[[4]], col= 'red') # 预测值
